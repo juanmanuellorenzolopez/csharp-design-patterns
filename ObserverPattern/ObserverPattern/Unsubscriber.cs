c34feb3a4929efa0933bf3f2790dcfb227f42344
Juan Manuel Lorenzo López <juanmanuel.lorenzolopez@gmail.com>
2023-12-30 08:12:33 +0100

    Observer Design Pattern .NET 8.0

---
+++ ObserverPattern/ObserverPattern/Unsubscriber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+    public class UnSubscriber(List<IObserver<Application>> observers, IObserver<Application> observer) : IDisposable
+    {
+        private readonly List<IObserver<Application>> _observers = observers;
+        private readonly IObserver<Application> _observer = observer;
+
+        public void Dispose()
+        {
+            _observers.Remove(_observer);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ ObserverPattern/Tests/ObserverPatternUnitTest.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObserverPattern;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class ObserverPatternUnitTest
+    {
+        [TestMethod]
+        public void WhenSubscribingToProvider_ThenProviderSendsNotifications()
+        {
+            var provider = new ApplicationsHandler();
+            var subscriber = new HRSpecialist("Bill");
+            subscriber.Subscribe(provider);
+            provider.AddApplication(new(1, "Sarah"));
+            Assert.IsTrue(subscriber.Applications.Any(app => app.JobId == 1 && string.Compare(app.ApplicantName, "Sarah") == 0));
+        }
+
+        [TestMethod]
+        public void WhenUnSubscribingToProvider_ThenSubscriberShouldHaveNoApplications()
+        {
+            var provider = new ApplicationsHandler();
+            var subscriber = new HRSpecialist("Bill");
+            subscriber.Subscribe(provider);
+            provider.AddApplication(new(1, "Sarah"));
+            subscriber.Unsubscribe();
+            Assert.IsFalse(subscriber.Applications.Any());
+        }
+
+        [TestMethod]
+        public void WhenApplicationsAreClosed_ThenSubscriberShouldReceiveNoMoreNotifications()
+        {
+            var provider = new ApplicationsHandler();
+            var subscriber = new HRSpecialist("Bill");
+            subscriber.Subscribe(provider);
+            provider.AddApplication(new(1, "Sarah"));
+            provider.CloseApplications();
+            provider.AddApplication(new(2, "Ahmed"));
+            Assert.IsFalse(subscriber.Applications.Any(app => app.JobId == 2 && string.Compare(app.ApplicantName, "Ahmed") == 0));
+        }
+    }
+}
